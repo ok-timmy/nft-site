f896bcdff7519d5a4331f15e4ea23c66f2754748
Timilehin Okunola <okunolatimilehin4123@gmail.com>
2023-03-21 17:57:57 +0100

    Tested minting function and wrote todos

--- Context/nftContext.tsx
+++ Context/nftContext.tsx
@@ -1,147 +1,145 @@
-import React, { useState, createContext, useEffect } from 'react'
-import { marketPlaceAddress, marketPlaceAbi } from '../nft_abis/marketPlaceAbi'
-import {
-  ipfsInterface,
-  marketPlaceInterface,
-  nft,
-  nftContextType,
-  nftType,
-} from '../Interfaces/nftType.interface'
-import { nftAbi, nftAddress } from '../nft_abis/nftDetails'
-import { ethers } from 'ethers'
-import { create } from 'ipfs-http-client'
-import { Buffer } from 'buffer'
+import React, { useState, createContext, useEffect } from "react";
+import { marketPlaceAddress, marketPlaceAbi } from "../nft_abis/marketPlaceAbi";
+import { nftType } from "../Interfaces/nftType.interface";
+import { nftAbi, nftAddress } from "../nft_abis/nftDetails";
+import { ethers } from "ethers";
+import { create } from "ipfs-http-client";
+import { Buffer } from "buffer";
+import { ipfsInterface } from "../Interfaces/ipfs.interface";
+import { nftContextType } from "../Interfaces/nftContext.type";
+import { marketPlaceInterface } from "../Interfaces/marketPlace.interface";
+import { nft } from "../Interfaces/nft.interface";
 
-const projectId = '2EEr1cONIKSUutMzqezxtVsJXFs'
+const projectId = "2EEr1cONIKSUutMzqezxtVsJXFs";
 // process.env.PROJECT_ID;   // <---------- your Infura Project ID
 
-const projectSecret = '072cc67028315165b77070123a12b998'
+const projectSecret = "072cc67028315165b77070123a12b998";
 // process.env.PROJECT_KEY;  // <---------- your Infura Secret
 
 const auth =
-  'Basic ' + Buffer.from(projectId + ':' + projectSecret).toString('base64')
+  "Basic " + Buffer.from(projectId + ":" + projectSecret).toString("base64");
 
 const client = create({
-  host: 'ipfs.infura.io',
+  host: "ipfs.infura.io",
   port: 5001,
-  protocol: 'https',
+  protocol: "https",
   headers: {
     authorization: auth,
   },
-})
+});
 
 type Props = {
-  children: React.ReactNode
-}
+  children: React.ReactNode;
+};
 
-export const nftContext = createContext<nftContextType | null>(null)
+export const nftContext = createContext<nftContextType | null>(null);
 
 const NftProvider = ({ children }: Props) => {
-  const [account, setAccount] = useState<string>('')
-  const [image, setImage] = useState<string>('')
-  const [name, setName] = useState<string>('')
-  const [price, setPrice] = useState(0)
-  const [category, setCategory] = useState<string>('')
-  const [items, setItems] = useState<Array<Object>>()
-  const [isLoading, setIsLoading] = useState(false)
-  const [purchases, setPurchases] = useState<any>()
-  const [description, setDescription] = useState('')
-  const [listedItems, setListedItems] = useState<Array<Object>>()
-  const [soldItems, setSoldItems] = useState<Array<Object>>()
-  const [marketPlace, setMarketPlace] = useState<marketPlaceInterface | any>()
-  const [nft, setNft] = useState<nft | any>()
+  const [account, setAccount] = useState<string>("");
+  const [image, setImage] = useState<string>("");
+  const [name, setName] = useState<string>("");
+  const [price, setPrice] = useState(0);
+  const [category, setCategory] = useState<string>("");
+  const [items, setItems] = useState<Array<Object>>();
+  const [isLoading, setIsLoading] = useState(false);
+  const [purchases, setPurchases] = useState<any>();
+  const [description, setDescription] = useState("");
+  const [listedItems, setListedItems] = useState<Array<Object>>();
+  const [soldItems, setSoldItems] = useState<Array<Object>>();
+  const [marketPlace, setMarketPlace] = useState<marketPlaceInterface | any>();
+  const [nft, setNft] = useState<nft | any>();
 
   //Metamask Connection
-  const web3Handler = async () : Promise<void> => {
+  const web3Handler = async (): Promise<void> => {
     const accounts = await (window as any).ethereum.request({
-      method: 'eth_requestAccounts',
-    })
-    setAccount(accounts[0])
+      method: "eth_requestAccounts",
+    });
+    setAccount(accounts[0]);
     // Get provider from metamask
-    const provider = new ethers.providers.Web3Provider((window as any).ethereum)
+    const provider = new ethers.providers.Web3Provider(
+      (window as any).ethereum
+    );
     // Get Signer
-    const signer = provider.getSigner()
-    console.log(signer)
-    console.log(marketPlaceAddress)
-    console.log(marketPlaceAbi)
-    loadContract(signer)
-    setIsLoading(false)
-  }
+    const signer = provider.getSigner();
+    console.log(signer);
+    console.log(marketPlaceAddress);
+    console.log(marketPlaceAbi);
+    loadContract(signer);
+    setIsLoading(false);
+  };
 
   //Load NFT Contract
-  const loadContract = async (signer: any) : Promise<void> => {
+  const loadContract = async (signer: any): Promise<void> => {
     const marketplaceContract = new ethers.Contract(
       marketPlaceAddress,
       marketPlaceAbi,
-      signer,
-    )
-    console.log(marketplaceContract)
-    console.log(marketPlaceAddress)
-    console.log(marketPlaceAbi)
-    setMarketPlace(marketplaceContract)
-    const nftContract = new ethers.Contract(nftAddress, nftAbi, signer)
-    setNft(nftContract)
-    setIsLoading(false)
-  }
-
-  
+      signer
+    );
+    console.log(marketplaceContract);
+    console.log(marketPlaceAddress);
+    console.log(marketPlaceAbi);
+    setMarketPlace(marketplaceContract);
+    const nftContract = new ethers.Contract(nftAddress, nftAbi, signer);
+    setNft(nftContract);
+    setIsLoading(false);
+  };
 
   // Mint Then List Function
-  const mintThenList = async (result: ipfsInterface) : Promise<void> => {
-    const uri = `https://ipfs.infura.io/ipfs/${result.path}`
+  const mintThenList = async (result: ipfsInterface): Promise<void> => {
+    const uri = `https://ipfs.infura.io/ipfs/${result.path}`;
     //Mint NFT
-    await nft?.mint(uri)
+    await nft?.mint(uri);
     // Get token Id for nft
-    const id = await nft?.tokenCount()
+    const id = await nft?.tokenCount();
     //Approve Marketplace to spend nft
-    let marketaddress = marketPlace?.address
-    let nftaddress = nft?.address
-    await (await nft?.setApprovalForAll(marketaddress as string, true))?.wait()
+    let marketaddress = marketPlace?.address;
+    let nftaddress = nft?.address;
+    await (await nft?.setApprovalForAll(marketaddress as string, true))?.wait();
     //Add nft to marketplace
-    const listingPrice = ethers.utils.parseEther(price.toString())
+    const listingPrice = ethers.utils.parseEther(price.toString());
 
-      await marketPlace?.makeItem(nftaddress as string, id, listingPrice)
-    .wait()
-
-  }
+    await marketPlace?.makeItem(nftaddress as string, id, listingPrice);
+  };
 
   // Create NFT Function
-  const createNFT = async () : Promise<void>  => {
-    if (!image || !price || !category || !name) return
+  const createNFT = async (): Promise<void> => {
+    if (!image || !price || !category || !name) return;
     setIsLoading(true);
     try {
       const result = await client.add(
-        JSON.stringify({ image, name, category }),
-      )
-      mintThenList(result)
+        JSON.stringify({ image, name, category })
+      );
+      mintThenList(result);
       setIsLoading(false);
     } catch (error) {
-      console.log('ipfs upload error: ', error)
+      console.log("ipfs upload error: ", error);
     }
-  }
+  };
 
   //Upload to IPFS Function
-  const uploadToIPFS = async (e: React.SyntheticEvent<EventTarget>) : Promise<void> => {
+  const uploadToIPFS = async (
+    e: React.SyntheticEvent<EventTarget>
+  ): Promise<void> => {
     // console.log(client);
-    e.preventDefault()
-    const file = e.target.files[0]
-    const reader = new window.FileReader()
-    reader.readAsArrayBuffer(file)
+    e.preventDefault();
+    const file = e.target.files[0];
+    const reader = new window.FileReader();
+    reader.readAsArrayBuffer(file);
     reader.onloadend = () => {
-      const res =  Buffer(reader.result)
-      console.log(res)
-    }
+      const res = Buffer(reader.result);
+      console.log(res);
+    };
 
-    if (typeof file !== 'undefined') {
+    if (typeof file !== "undefined") {
       try {
-        const result = await client.add(file)
-        console.log(result)
-        setImage(`https://ipfs.infura.io/ipfs/${result.path}`)
+        const result = await client.add(file);
+        console.log(result);
+        setImage(`https://ipfs.infura.io/ipfs/${result.path}`);
       } catch (error) {
-        console.log('IPFS Image Upload error: ', error)
+        console.log("IPFS Image Upload error: ", error);
       }
     }
-  }
+  };
 
   //Load Purchased Items
   const loadPurchasedItems = async (): Promise<void> => {
@@ -153,23 +151,23 @@ const NftProvider = ({ children }: Props) => {
       null,
       null,
       null,
-      account,
-    )
-    const results = await marketPlace.queryFilter(filter)
+      account
+    );
+    const results = await marketPlace.queryFilter(filter);
 
     //fetch metadata of each nft and add that to listedItems object
     const purchases = await Promise.all(
       results.map(async (i: any) => {
         //Fetch argument from each result
-        i = i.args
+        i = i.args;
         //Get uri url from nft contract
-        const uri = await nft.tokenURI(i.tokenId)
+        const uri = await nft.tokenURI(i.tokenId);
         //use uri to fetch the nft metadata stored on ipfs
-        const response = await fetch(uri)
-        const metadata = await response.json()
+        const response = await fetch(uri);
+        const metadata = await response.json();
 
         //Get total price of item(item fee + price)
-        const totalPrice = await marketPlace.getTotalPrice(i.itemId)
+        const totalPrice = await marketPlace.getTotalPrice(i.itemId);
 
         let purchasedItems = {
           totalPrice,
@@ -178,32 +176,32 @@ const NftProvider = ({ children }: Props) => {
           name: metadata.name,
           description: metadata.description,
           image: metadata.image,
-        }
+        };
 
-        return purchasedItems
-      }),
-    )
-    setPurchases(purchases)
-    setIsLoading(false)
-  }
+        return purchasedItems;
+      })
+    );
+    setPurchases(purchases);
+    setIsLoading(false);
+  };
 
   //Load Listed Items
   const loadListedItems = async (): Promise<void> => {
-    const itemCount = await marketPlace?.itemCount()
-    let listedItems = []
-    let soldItems = []
+    const itemCount = await marketPlace?.itemCount();
+    let listedItems = [];
+    let soldItems = [];
 
     for (let index = 0; index < itemCount; index++) {
-      const i = await marketPlace?.items(index)
+      const i = await marketPlace?.items(index);
       if (i.seller.toLowerCase() === account) {
         // Get URI URL from nft contract
-        const uri = await nft?.tokenURL(i.tokenId)
+        const uri = await nft?.tokenURL(i.tokenId);
         //use the uri to fetch metadata stored on ipfs
-        const response = await fetch(uri)
-        const metadata = await response.json()
+        const response = await fetch(uri);
+        const metadata = await response.json();
 
         //Get total price of item
-        const totalPrice = await marketPlace?.getTotalPrice(i.itemId)
+        const totalPrice = await marketPlace?.getTotalPrice(i.itemId);
         //Define Listed item object
         let item = {
           totalPrice,
@@ -212,34 +210,34 @@ const NftProvider = ({ children }: Props) => {
           name: metadata.name,
           description: metadata.description,
           image: metadata.image,
-        }
+        };
 
-        listedItems.push(item)
-        if (i.sold) soldItems.push(item)
+        listedItems.push(item);
+        if (i.sold) soldItems.push(item);
       }
     }
 
-    setListedItems(listedItems)
-    setSoldItems(soldItems)
-    setIsLoading(false)
-  }
+    setListedItems(listedItems);
+    setSoldItems(soldItems);
+    setIsLoading(false);
+  };
 
   //Load MarketPlace Items
-  const loadMarketPlaceItems = async () : Promise<void> => {
-    console.log(marketPlace)
-    const itemCount = await marketPlace?.itemCount()
-    let itemsArray = []
+  const loadMarketPlaceItems = async (): Promise<void> => {
+    console.log(marketPlace);
+    const itemCount = await marketPlace?.itemCount();
+    let itemsArray = [];
     for (let i = 1; i < itemCount.length; i++) {
-      const item = await marketPlace?.items(i)
+      const item = await marketPlace?.items(i);
       if (!item.sold) {
         //get uri url from nft contract
-        const uri = await nft?.tokenURI(item.tokenId)
+        const uri = await nft?.tokenURI(item.tokenId);
         //use uri to fetch nft metadata stored on ipfs
-        const response = await fetch(uri)
-        const metadata = await response.json()
+        const response = await fetch(uri);
+        const metadata = await response.json();
 
         //get total price
-        const totalPrice = await marketPlace?.getTotalPrice(item.itemId)
+        const totalPrice = await marketPlace?.getTotalPrice(item.itemId);
         // Add Item to items array
         itemsArray.push({
           totalPrice,
@@ -248,20 +246,20 @@ const NftProvider = ({ children }: Props) => {
           name: metadata.name,
           description: metadata.description,
           image: metadata.image,
-        })
+        });
       }
     }
-    setItems(itemsArray)
-    setIsLoading(false)
-  }
+    setItems(itemsArray);
+    setIsLoading(false);
+  };
 
   // Buy NFT Item
   const buyMarketitem = async (item: nftType): Promise<void> => {
     await (
       await marketPlace.buyItem(item.itemId, { value: item.totalPrice })
-    ).wait()
-    loadMarketPlaceItems()
-  }
+    ).wait();
+    loadMarketPlaceItems();
+  };
 
   return (
     <nftContext.Provider
@@ -293,7 +291,7 @@ const NftProvider = ({ children }: Props) => {
     >
       {children}
     </nftContext.Provider>
-  )
-}
+  );
+};
 
-export default NftProvider
+export default NftProvider;
--- README.md
+++ README.md
@@ -32,3 +32,10 @@ You can check out [the Next.js GitHub repository](https://github.com/vercel/next
 The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
 
 Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+
+
+# TODO
+Make a loading overlay that displays until all transaction is completed
+Empty the form on the create page immediately the minting and listing is successful
+Handle the errors properly and use display appropriate messages for each error rather than the app just crashing
+Implement cypress test to the app.
